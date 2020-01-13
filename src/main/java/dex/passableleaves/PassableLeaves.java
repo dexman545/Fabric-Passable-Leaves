779839f8e63d803dede43af27db0184e01323652
dexman545 <kev12east@gmail.com>
2020-01-13 13:38:28 -0500

    # Conflicts: #	gradle.properties #	src/main/java/dex/passableleaves/mixin/LeavesBlockMixin.java

--- src/main/java/dex/passableleaves/PassableLeaves.java
+++ src/main/java/dex/passableleaves/PassableLeaves.java
@@ -1,11 +1,70 @@
 package dex.passableleaves;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.server.ServerStartCallback;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.registry.Registry;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 
 public class PassableLeaves implements ModInitializer {
+	public static String config = FabricLoader.getInstance().getConfigDirectory().toString() + "/passable-leaves.cfg";
+	private static final File bob = new File(config);
+	public static List<String> meh = new ArrayList<String>();
+
+	public static Optional<List<String>> getPassable() {
+
+		ArrayList<String> b = new ArrayList<String>();
+
+		//generate config file; removes incorrect values from existing one as well
+		try {
+			if (!bob.exists()) {
+				FileUtils.openOutputStream(bob).write(("minecraft:oak_leaves\n" +
+						"minecraft:spruce_leaves\n" +
+						"minecraft:birch_leaves\n" +
+						"minecraft:jungle_leaves\n" +
+						"minecraft:acacia_leaves\n" +
+						"minecraft:dark_oak_leaves\n").getBytes());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Scanner s = new Scanner(bob);
+			while (s.hasNext()) {
+				b.add(s.next());
+			}
+			return Optional.of(b);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+
+		return Optional.empty();
+	}
+
 	@Override
 	public void onInitialize() {
 
-		System.out.println("Hello Fabric world!");
+		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) ->
+		{
+
+			System.out.println(Registry.BLOCK.getId(world.getBlockState(pos).getBlock()));
+
+			return ActionResult.PASS;
+		});
+
+		ServerStartCallback.EVENT.register(t -> {
+			meh.clear();
+			meh.addAll(Objects.requireNonNull(getPassable().orElse(null)));
+		});
+
 	}
 }
--- src/main/java/dex/passableleaves/mixin/LeavesBlockMixin.java
+++ src/main/java/dex/passableleaves/mixin/LeavesBlockMixin.java
@@ -1,60 +1,85 @@
 package dex.passableleaves.mixin;
 
+import dex.passableleaves.PassableLeaves;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.LeavesBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LeavesBlock.class)
-public class LeavesBlockMixin extends Block {
+import java.util.List;
 
-	@Inject(method = "<init>(Lnet/minecraft/block/Block$Settings;)V", at = @At("RETURN"))
-	public void LeavesBlockBlah(Settings settings, CallbackInfo ci) {
-		((BlockAccessMixin) this).setCollidable(false);
+@Mixin(BlockState.class)
+public abstract class LeavesBlockMixin {
+
+	@Shadow public abstract Block getBlock();
+
+	private List<String> getPassable() {
+		return PassableLeaves.meh;
 	}
 
-	public LeavesBlockMixin(Settings settings) {
-		super(settings.noCollision());
-	}
-
-	@Override
-	public float getAmbientOcclusionLightLevel(BlockState state, BlockView view, BlockPos pos) {
-		return 0.2f;
-	}
-
-	@Override
-	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-		entity.fallDistance = entity.fallDistance * 0.95f;
-		Vec3d oldVel = entity.getVelocity();
-		if (entity instanceof PlayerEntity) {
-			if (((PlayerEntity) entity).isFallFlying()) {
-				//can't reduce horizontal velocity otherwise no flight is possible
-				entity.setVelocity(oldVel.multiply(1.0D, (oldVel.y > 0 ? 1.0D : 0.5D), 1.0D));
-				entity.damage(DamageSource.FLY_INTO_WALL, (float) (entity.getVelocity().length() / .25D) * 0.5f);
-			} else {
-				entity.setVelocity(oldVel.multiply(0.8D, (oldVel.y > 0 ? 1.0D : 0.5D), 0.8D));
+	@Inject(at=@At("HEAD"), method = "getCollisionShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/EntityContext;)Lnet/minecraft/util/shape/VoxelShape;", cancellable = true)
+	public void getCollisionShape(BlockView view, BlockPos pos, EntityContext ePos, CallbackInfoReturnable<VoxelShape> cir) {
+		//for (String id : PassableLeaves.CONFIG.passableBlocks()) {
+		//if (!getPassable().isEmpty()) {
+			if (BlockTags.LEAVES.contains(this.getBlock()) || getPassable().contains(Registry.BLOCK.getId(this.getBlock()).toString())) {
+			//if (Registry.BLOCK.getId(this.getBlock()).equals(Identifier.tryParse(PassableLeaves.CONFIG.passableBlocks().get(0)))) {
+				cir.setReturnValue(VoxelShapes.empty());
+				//}
 			}
-		} else if (entity instanceof ProjectileEntity) {
-			entity.setVelocity(oldVel.multiply(0.5D));
-
-		} else {
-			entity.setVelocity(oldVel.multiply(0.7D, (oldVel.y > 0 ? 1.0D : 0.5D), 0.7D));
-		}
-		if (entity.getVelocity().length() > 0.1D) {
-			entity.handleFallDamage(entity.fallDistance, 0.5f);
-		}
+		//}
+		//}
 	}
+
+	@Inject(at=@At("HEAD"), method = "getCollisionShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;", cancellable = true)
+	public void getCollisionShape(BlockView view, BlockPos pos, CallbackInfoReturnable<VoxelShape> cir) {
+		if (BlockTags.LEAVES.contains(this.getBlock()) || getPassable().contains(Registry.BLOCK.getId(this.getBlock()).toString())) {
+				cir.setReturnValue(VoxelShapes.empty());
+			}
+	}
+
+	@Inject(at=@At("HEAD"), method = "onEntityCollision")
+	public void onEntityCollision(World world, BlockPos pos, Entity entity, CallbackInfo ci) {
+		if (BlockTags.LEAVES.contains(this.getBlock()) || getPassable().contains(Registry.BLOCK.getId(this.getBlock()).toString())) {
+				entity.fallDistance = entity.fallDistance * 0.95f;
+				Vec3d oldVel = entity.getVelocity();
+				if (entity instanceof PlayerEntity) {
+					if (((PlayerEntity) entity).isFallFlying()) {
+						//can't reduce horizontal velocity otherwise no flight is possible
+						entity.setVelocity(oldVel.multiply(1.0D, (oldVel.y > 0 ? 1.0D : 0.5D), 1.0D));
+						entity.damage(DamageSource.FLY_INTO_WALL, (float) (entity.getVelocity().length() / .25D) * 0.5f);
+					} else {
+						entity.setVelocity(oldVel.multiply(0.8D, (oldVel.y > 0 ? 1.0D : 0.5D), 0.8D));
+					}
+				} else if (entity instanceof ProjectileEntity) {
+					entity.setVelocity(oldVel.multiply(0.5D));
+
+				} else {
+					entity.setVelocity(oldVel.multiply(0.7D, (oldVel.y > 0 ? 1.0D : 0.5D), 0.7D));
+				}
+				if (entity.getVelocity().length() > 0.1D) {
+					entity.handleFallDamage(entity.fallDistance, 0.5f);
+				}
+			}
+	}
+
+
 
 }
 
